a48a50ad40b75a31d97684a575753d5d0f73e1f3
vareIIa <jvvarella@hotmail.com>
2024-03-15 12:53:40 -0300

    Update

--- src/screens/site-nota/resultadoPAC.jsx
+++ src/screens/site-nota/resultadoPAC.jsx
@@ -1,36 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import { Box, useMediaQuery, useTheme, Grid, Fade, CircularProgress, Typography } from '@mui/material';
-import TabelaAluno from '../../components/Tabela/TabelaData'; // Renomeado para evitar conflitos
+import Tabela from '../../components/Tabela/Tabela';
 import PesquisarO from '../../components/Pesquisar/PesquisarO';
 import Sobre from '../../components/Sobre/Sobre';
 import PDLOGO from '../../components/PDLOGO/PDLOGO3';
 import Navbar from '../../components/Navbar/Navbar';
 
+import axios from 'axios';
+
 const Aluno = () => {
   const theme = useTheme();
   const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
   const isMobile = useMediaQuery('(max-width:600px)');
-  const [alunoData, setAlunoData] = useState(null);
+  const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setLoading(true);
-        const response = await fetch('https://gist.githubusercontent.com/vareIIa/ad98f88cd7dff2855868c54065b5ccbe/raw/adf5e3881f0aa884730df4d9cb013e34204444bf/Tabela');
-        const textData = await response.text();
-        console.log(textData); // Verifique os dados no console
-        setAlunoData(textData); // Atualizado para refletir o novo nome do estado
-        setLoading(false);
-      } catch (error) {
-        setError('Erro ao buscar os dados.');
-        setLoading(false);
-        console.error('Error fetching data: ', error);
-      }
-    };
 
-    fetchData();
   }, []);
 
   return (
@@ -50,9 +37,7 @@ const Aluno = () => {
       >
         <Navbar />
         <Fade>
-
           <PDLOGO/>
-    
         </Fade>   
         <Sobre />
         <Grid item xs={12} md={6}>
@@ -63,6 +48,7 @@ const Aluno = () => {
             alignItems: 'center',
             pt: 0
           }}>
+            {/* Verifica se é tela móvel para renderizar ou não o componente PesquisarO */}
             {isMobile && <PesquisarO />}
           </Box>
         </Grid>
@@ -75,8 +61,8 @@ const Aluno = () => {
             <Typography variant="body1" color="error">{error}</Typography>
           ) : (
             <Box>
-              {/* Renderize a tabela aqui */}
-              <TabelaAluno data={alunoData} /> {/* Atualizado para refletir o novo nome do componente */}
+              {/* Verifica se é tela móvel para renderizar ou não a tabela */}
+              {!isMobile && <Tabela data={data} />}
             </Box>
           )}
         </Grid>
